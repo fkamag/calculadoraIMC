e12837ce09e2160af40fdc5f954598067259d10f
fkamag <fabiokamagian@gmail.com>
2024-01-29 15:20:33 -0300

    testa baixo peso grave

--- src/test/TestPaciente.java
+++ src/test/TestPaciente.java
@@ -2,33 +2,35 @@ package test;
 
 import static java.lang.Math.round;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import main.Paciente;
-import main.Principal;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 public class TestPaciente {
 
   @Test
-  @DisplayName("1 - Testa peso muito baixo")
+  @Order(1)
+  @DisplayName("1 - Testa Baixo peso muito grave")
   public void testImcBaixoPesoMuitoGrave() {
-
     Paciente paciente = new Paciente(40, 1.60);
-    assertEquals(15.62, round(paciente.calcularIMC(paciente.getPeso(),
+    double result = paciente.calcularIMC(paciente.getPeso(), paciente.getAltura());
+    String diagnostic = paciente.diagnostico(result);
+    assertEquals(15.63, round(result), 2);
+    assertEquals("Baixo peso muito grave", diagnostic);
+  }
+
+  @Test
+  @Order(2)
+  @DisplayName("2 - Testa Baixo peso grave")
+  public void testImcBaixoPesoGrave() {
+    Paciente paciente = new Paciente(50, 1.72);
+    double result = paciente.calcularIMC(paciente.getPeso(), paciente.getAltura());
+    String diagnostic = paciente.diagnostico(result);
+    assertEquals(16.91, round(paciente.calcularIMC(paciente.getPeso(),
         paciente.getAltura())), 2);
-
-    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-    PrintStream printStream = new PrintStream(baos);
-    System.setOut(printStream);
-    Principal.main(null);
-
-    String output = baos.toString();
-    assertTrue(output.contains("Baixo peso muito grave"));
-
+    assertEquals("Baixo peso grave", diagnostic);
   }
 
 }
